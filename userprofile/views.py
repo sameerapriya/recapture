bb6f4e7b528e7b5a77a583355a8b38814ce93269
sameerapriya <sameerakorivi@yahoo.com>
2020-07-18 22:57:35 +0530

    auth template fixed

--- userprofile/views.py
+++ userprofile/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,template_name='base.html')
+    return render(request,template_name='layout.html')
